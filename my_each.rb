4638cdc32bcd3fcde6087cd85ebb9f5235922a62
hpetrow <heather.petrow@gmail.com>
2015-06-04 20:10:59 -0400

    done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  counter = 0
+  new_arr = []
+  while(counter < arr.length)
+    new_arr << yield(arr[counter])
+    counter += 1
+  end
+  arr
 end
